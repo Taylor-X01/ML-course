f91edfaf7167233f839a38a9c489e14d35301671
grini anass <grini.anass1@gmail.com>
2022-01-23 17:43:50 +0100

    Update the Logistic regression model

--- TP2/logisticReg.py
+++ TP2/logisticReg.py
@@ -1,5 +1,6 @@
 import numpy as np
 import copy as cp
+import matplotlib.pyplot as plt
 
 class LogisticRegression:
 
@@ -8,21 +9,21 @@ class LogisticRegression:
         self.n_iters = n_iter
         self.weights = None
         self.bias = None
+        self.loss = 1e+100
 
     def fit(self,X,y):
         n_samples, n_features = X.shape
-
         # init parameters
         self.weights = np.zeros(n_features)
         self.bias = 0
-
         # gradient descent
-        for _ in range(self.n_iters):
+        for i in range(self.n_iters):
             # approximate y with linear combination of weights and x, plus bias
             linear_model = np.dot(X, self.weights) + self.bias
             # apply sigmoid function
             y_predicted = self._sigmoid(linear_model)
-
+            self.loss = self.cross_entropy(y,y_predicted)
+            
             # compute gradients
             dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
             db = (1 / n_samples) * np.sum(y_predicted - y)
@@ -31,10 +32,12 @@ class LogisticRegression:
             self.bias -= self.lr * db
         
 
-    def predict(self,X):
+    def predict(self,X, verbose=False):
         linear_model = np.dot(X, self.weights) + self.bias
         y_predicted = self._sigmoid(linear_model)
         y_predicted_cls = [1 if i > 0.5 else 0 for i in y_predicted]
+        if verbose:
+            print(f"sigmoid_output : {y_predicted.shape}")
         return np.array(y_predicted_cls)
     
     
